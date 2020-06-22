ae044510e0c1db82155b1ec299fcba616a2558c9
Jonathan Rhone <rhone.j@gmail.com>
2020-06-22 08:12:26 -0400

    add pyvis

--- html/component.py
+++ html/component.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import hiplot as hip
+import pandas as pd
+import networkx as nx
 import folium
-import nglview
 import time
 import io
-
-# Need full screen support on html
-# Need refresh without reloading components
+from pyvis.network import Network
 
 # Load HiPlot chart - reload causes entire widget to flicker
 h = hip.Experiment.from_csv("hiplot-selected-6968.csv")
@@ -16,8 +15,28 @@ st.html(h.to_html(), height=700) #  key="a")
 m = folium.Map(location=[45.5236, -122.6750])
 st.html(m._repr_html_(), height=700) #, key="y")
 
-# Load NGLView - jupyter notebook widget, some script loading issue
-output = io.StringIO()
-view = nglview.show_pdbid('1tsu')
-nglview.write_html(output, [view])
-st.html(output.getvalue())
+# Load Pyvis network
+got_net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white")
+got_net.barnes_hut()
+got_data = pd.read_csv("https://www.macalester.edu/~abeverid/data/stormofswords.csv")
+
+sources = got_data['Source']
+targets = got_data['Target']
+weights = got_data['Weight']
+edge_data = zip(sources, targets, weights)
+
+for e in edge_data:
+    src = e[0]
+    dst = e[1]
+    w = e[2]
+    got_net.add_node(src, src, title=src)
+    got_net.add_node(dst, dst, title=dst)
+    got_net.add_edge(src, dst, value=w)
+
+neighbor_map = got_net.get_adj_list()
+for node in got_net.nodes:
+    node["title"] += " Neighbors:<br>" + "<br>".join(neighbor_map[node["id"]])
+    node["value"] = len(neighbor_map[node["id"]])
+
+got_net.write_html("gameofthrones.html")
+st.html(got_net.html, height=700)
